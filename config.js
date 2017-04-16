24111557eb473362beaec2c03ec73bbf30a80222
garytryan <gary.thomas.ryan@gmail.com>
2017-04-16 09:44:13 -0700

    serving production app

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// "cdn": "http://d2xwk3exi9tre0.cloudfront.net",
-
 module.exports = () => ({
-  "cdn": "http://next.zine.media.s3-website-us-west-1.amazonaws.com",
+  "cdn": "http://d2xwk3exi9tre0.cloudfront.net",
   "facebookAppId": "826483897470671",
   "zineAPI": process.env.ZINE_API || "http://localhost:3001"
 })
