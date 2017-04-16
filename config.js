ef6eee6213360373d7160dc081ece5ffa8d906c3
garytryan <gary.thomas.ryan@gmail.com>
2017-04-15 18:14:11 -0700

    testing with next

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
+// "cdn": "http://d2xwk3exi9tre0.cloudfront.net",
+
 module.exports = () => ({
-  "cdn": "http://d2xwk3exi9tre0.cloudfront.net",
+  "cdn": "http://next.zine.media.s3-website-us-west-1.amazonaws.com",
   "facebookAppId": "826483897470671",
   "zineAPI": process.env.ZINE_API || "http://localhost:3001"
 })
